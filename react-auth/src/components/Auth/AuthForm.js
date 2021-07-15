59b73c05bb491519179ed3d9d401ce273a5258aa
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-07-15 18:48:43 +0530

    Adding user signup, firebase setup, showing result feedback to user

--- react-auth/src/components/Auth/AuthForm.js
+++ react-auth/src/components/Auth/AuthForm.js
@@ -1,34 +1,84 @@
-import { useState } from 'react';
+import { useRef, useState } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault(); //We are sending requests and we dont want browser to send requests continuosly
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    setLoading(true);
+    if (isLogin) {
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB6Bxlw6FbRjFMWvit-BQEwGRhpPeLdLs4",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => {
+        setLoading(false);
+        if (res.ok) {
+          console.log("response is:", res);
+        } else {
+          return res.json().then((data) => {
+            console.log("error is:", data);
+            let errorMessage = "Authentication failed!";
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            alert(errorMessage);
+          });
+        }
+      });
+    }
+  };
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!loading && <button>{isLogin ? "Login" : "Create Account"}</button>}
+          {loading && <button>Loading....</button>}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
@@ -37,3 +87,6 @@ const AuthForm = () => {
 };
 
 export default AuthForm;
+
+// DOC: https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
+// API KEY: settings => Project settings => web api
