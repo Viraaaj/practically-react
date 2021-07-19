568c359f085f9fed689dbac03f6042892cab130d
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-07-19 19:09:52 +0530

    Error in auth-context, not redirecting when entered right creds

--- react-auth/src/store/auth-context.js
+++ react-auth/src/store/auth-context.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+
+let logoutTimer;
 
 const AuthContext = React.createContext({
   token: "",
@@ -15,27 +17,64 @@ const calculateRemainingTokenTime = (expirationTime) => {
   return remainingTime;
 };
 
+const retriveStoredToken = () => {
+  const storedToken = localStorage.getItem("token");
+  const storedExpirationDate = localStorage.getItem("expirationTime");
+
+  const remainingTime = calculateRemainingTokenTime(storedExpirationDate);
+
+  if (remainingTime <= 360000) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTime");
+    return null;
+  }
+
+  return {
+    token: storedToken,
+    duration: remainingTime,
+  };
+};
+
 export const AuthContextProvider = (props) => {
-  const initialToken = localStorage.getItem("token");
+  const tokenData = retriveStoredToken();
+
+  let initialToken;
+
+  if (tokenData) {
+    initialToken = tokenData.token;
+  }
 
   const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token; //!! will return bboolean value of truthy or falsy value
 
-  const logoutHandler = () => {
+  const logoutHandler = useCallback(() => {
     setToken(null);
-    localStorage.removeItem("token", token);
-  };
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTime");
+
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  }, []);
 
   const loginHandler = (token, expirationTime) => {
     setToken(token);
     localStorage.setItem("token", token);
+    localStorage.setItem("expirationTime", expirationTime);
 
     const remainingTime = calculateRemainingTokenTime(expirationTime);
 
-    // setTimeout(loginHandler, 3000);
-    setTimeout(loginHandler, remainingTime);
+    // setTimeout(logoutHandler, 3000);
+    logoutTimer = setTimeout(logoutHandler, remainingTime);
   };
+
+  useEffect(() => {
+    if (tokenData) {
+      console.log("token duration:", tokenData.duration);
+      logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+    }
+  }, [tokenData, logoutHandler]);
 
   const contextValue = {
     token: token,
